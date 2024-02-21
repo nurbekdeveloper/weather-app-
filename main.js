60e4a4e40c1fde21dc76e6148454a5cfa617d6c1
nurbekdeveloper <nurbeknormatov_02@gmail.com>
2024-02-21 09:06:07 +0500

    add new flies

---
+++ main.js
@@ -0,0 +1,42 @@
+let input = document.querySelector("#input");
+let manzil = document.querySelector("#location");
+let gradus= document.querySelector("#gradus");
+let type = document.querySelector("#type");
+let btn = document.querySelector("#btn")
+
+ const apKey = "820b05bd9bcb9184bba3c8c9a15c560c"
+
+
+
+
+btn.addEventListener('click' , ()=>{
+  const address =input.value;
+  if(address){
+    fetchWeather(address)
+
+  }
+})
+
+
+
+
+ function fetchWeather(address) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&appid=${apKey}`;
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    manzil.innerHTML=data.name;
+    gradus.innerHTML=`${Math.round(data.main.temp)}°C`;
+    type.innerHTML=data.weather[0].description;
+
+  })
+  .catch(error=>{
+    console.error('Error fetching weather data:', error);
+  })
+
+
+
+
+
+ }
